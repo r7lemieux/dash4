4260b2ddf3b9ac39b9b6ee62b5a2b18ec54a9ca6
Richard Lemieux <r7lemieux@gmail.com>
2015-05-04 17:04:55 -0500

    start and cheat

---
+++ server/.server/model/member.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * Member Model
+ *
+ * Created by r7lemieux on 1/26/15.
+ */
+
+var Member = function Member(data) {
+
+  this.name  = data.name;
+
+};
+
+exports.Member = Member;
+
+
+
---
+++ start/public/.public/controllers/homeCtr.js
@@ -0,0 +1,7 @@
+'use strict';
+
+angular.module('dash').controller('HomeCtr', [
+  function () {
+  }
+])
+;
