a81b66f4f0d7b405ca908b223873d9178ec4e050
Richard Lemieux <r7lemieux@gmail.com>
2015-05-04 17:21:47 -0500

    remove key

--- server/.server/server/controllers/memberController.js
+++ server/.server/server/controllers/memberController.js
@@ -11,11 +11,11 @@
 var
   Member = require('../model/member'),
   _ = require('lodash'),
-  memberService = require('../services/memberService')
+  meetupService = require('../services/meetupService')
   ;
 
 exports.getAll = function (req, res) {
-  memberService.getAllMembers(function (members) {
+  meetupService.getAllMembers(function (members) {
     //res.status(200);
     res.send(members);
   });
@@ -25,20 +25,20 @@ exports.get = function (req, res) {
   var
     id = req.query.memberId;
 
-  memberService.getMemberById(id, function (member) {
+  meetupService.getMemberById(id, function (member) {
     res.jsonp(member);
 
   });
 };
 
 exports.update = function(req, res) {
-  memberService.getMemberById(req.body.id, function(member) {
+  meetupService.getMemberById(req.body.id, function(member) {
     var newMember = req.body;
     _.forEach(newMember, function(val, key) {
       console.log('101 key ' + key + ' val ' + val);
       member[key] = val;
     });
-    memberService.persistMembers();
+    meetupService.persistMembers();
     res.send(member);
   });
 };
--- start/public/.public/public/controllers/homeCtr.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-angular.module('dash').controller('HomeCtr', function ($scope) {
-      $scope.welcomeText = 'Welcome to DASH-4 workshop !';
-    }
-)
-
